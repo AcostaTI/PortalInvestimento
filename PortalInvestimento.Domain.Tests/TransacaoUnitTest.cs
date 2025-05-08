adbbc12d12684d00b49650ebb29e7bca152d7a8d
Alexandre Maciel <ti.alexandre.costa@gmail.com>
2025-05-08 19:08:27 -0300

    teste CI

--- PortalInvestimento.Domain.Tests/TransacaoUnitTest.cs
+++ PortalInvestimento.Domain.Tests/TransacaoUnitTest.cs
@@ -14,14 +14,5 @@ namespace PortalInvestimento.Domain.Tests
             action.Should().NotThrow<DomainException>();
         }
 
-        [Fact]
-        public void CreateTransacao_OperacaoComMaisDeUmcaractere_RetornoExcception()
-        {
-            Action action = () => new Transacao(1, 2, 1.5M, DateTime.Now, 1, 1, "AR");
-
-            action.Should().Throw<DomainException>().WithMessage("Não deve ser maior que 1 caractere.");
-
-        }
-
     }
 }
