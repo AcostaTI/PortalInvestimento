e98d3f164955bfc648667470e5510ec2aec300fe
Alexandre Maciel <ti.alexandre.costa@gmail.com>
2025-05-14 23:27:49 -0300

    portifolio unit test prova

--- PortalInvestimento.Domain.Tests/PortfolioUnitTest.cs
+++ PortalInvestimento.Domain.Tests/PortfolioUnitTest.cs
@@ -8,36 +8,36 @@ namespace PortalInvestimento.Domain.Tests
 {
     public class PortfolioUnitTest
     {
-        //Usuario usuario = new Usuario() 
-        //{
-        //    Nome = "Usuario Teste",
-        //    Email = "Email@Teste.com",
-        //    Senha = "SenhaTeste",
-        //    TipoAcesso = EnTipoAcesso.Admin
-        //};
-        //[Fact]
-        //public void CreatePortfolio_ParametrosValidos_RetornoValido()
-        //{
-        //    Action action = () => new Portfolio("Portfolio1", "Descricao do Portfolio", "P1", 1, usuario);
-        //    action.Should().NotThrow<DomainException>();
-        //}
-        //[Fact]
-        //public void CreatePortfolio_CodigoNotEmpty_RetornoExcception()
-        //{
-        //    Action action = () => new Portfolio("", "Descricao do Portfolio", "P1", 0, usuario);
-        //    action.Should().Throw<DomainException>().WithMessage("Nome precisa ser preenchido.");
-        //}
-        //[Fact]
-        //public void CreatePortfolio_NomeNotEmpty_RetornoExcception()
-        //{
-        //    Action action = () => new Portfolio("Portfolio1", "Descricao do Portfolio", "", 0, usuario);
-        //    action.Should().Throw<DomainException>().WithMessage("Codigo precisa ser preenchido."); 
-        //}
-        //[Fact]
-        //public void CreatePortfolio_DescricaoNotEmpty_RetornoExcception()
-        //{
-        //    Action action = () => new Portfolio("Portfolio1", "", "P1", 0, usuario);
-        //    action.Should().Throw<DomainException>().WithMessage("Descricao precisa ser preenchido.");
-        //}
+        Usuario usuario = new Usuario()
+        {
+            Nome = "Usuario Teste",
+            Email = "Email@Teste.com",
+            Senha = "SenhaTeste",
+            TipoAcesso = EnTipoAcesso.Admin
+        };
+        [Fact]
+        public void CreatePortfolio_ParametrosValidos_RetornoValido()
+        {
+            Action action = () => new Portfolio("Portfolio1", "Descricao do Portfolio", "P1", 1, usuario);
+            action.Should().NotThrow<DomainException>();
+        }
+        [Fact]
+        public void CreatePortfolio_CodigoNotEmpty_RetornoExcception()
+        {
+            Action action = () => new Portfolio("", "Descricao do Portfolio", "P1", 0, usuario);
+            action.Should().Throw<DomainException>().WithMessage("Nome precisa ser preenchido.");
+        }
+        [Fact]
+        public void CreatePortfolio_NomeNotEmpty_RetornoExcception()
+        {
+            Action action = () => new Portfolio("Portfolio1", "Descricao do Portfolio", "", 0, usuario);
+            action.Should().Throw<DomainException>().WithMessage("Codigo precisa ser preenchido.");
+        }
+        [Fact]
+        public void CreatePortfolio_DescricaoNotEmpty_RetornoExcception()
+        {
+            Action action = () => new Portfolio("Portfolio1", "", "P1", 0, usuario);
+            action.Should().Throw<DomainException>().WithMessage("Descricao precisa ser preenchido.");
+        }
     }
 }
